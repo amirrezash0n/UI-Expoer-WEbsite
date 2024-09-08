737f8c2069f5dd3421d5a6fa0fddd7720582499d
amirrezash0n <159563194+amirrezash0n@users.noreply.github.com>
2024-09-08 21:55:24 +0330

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,26 @@ navToggleIcon.addEventListener('click', function() {
     this.classList.toggle('hamburger-menu--open');
     menu.classList.toggle('menu--open');
 });
+
+const servicesTitle = document.querySelector(".services__title")
+
+let servicesTitleTypewriter = new Typewriter(servicesTitle, {
+  loop: true,
+});
+
+servicesTitleTypewriter
+
+    .typeString("We Provide Best Quality Service")
+    .pauseFor(2500)
+    .deleteChars(20)
+    .pauseFor(2500)
+
+    .typeString("Superior Service")
+    .pauseFor(2500)
+    .deleteChars(16)
+
+    .typeString("Premium Service")
+    .pauseFor(2500)
+    .deleteChars(15)
+
+    .start();
